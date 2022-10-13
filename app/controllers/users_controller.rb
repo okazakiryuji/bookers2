dc5dbd39525ac050060563f6ab998d6448958c48
EC2 Default User <ec2-user@ip-172-31-1-15.ap-northeast-1.compute.internal>
2022-10-13 12:38:25 +0000

    bootstrapを導入

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  def show
+    @user =
+    @post_images = @user.post_images
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  get 'books/new'
+  get 'homes/top'
+  root to: 'homes#top'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :post_images, only: [:new, :index, :show]
 end
