e2fc8cf4cec3fc26189188bca6de31ad2c0339ce
EC2 Default User <ec2-user@ip-172-31-1-15.ap-northeast-1.compute.internal>
2022-10-19 13:13:55 +0000

    indexページ編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,11 +4,18 @@ class BooksController < ApplicationController
 
   def index
     @Book = Book.new
+    @books = Book.all
+
   end
 
   def create
-    book = book.new(book_params)
+    book = Book.new(book_params)
+    book.user_id = current_user.id
     book.save
     redirect_to '/books'
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :body,:image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books
   resources :users
-  post 'users' =>'users#create'
+  post 'books' =>'books#create'
 end
